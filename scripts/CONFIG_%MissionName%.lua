88b561ffae24e8b20dfc97f1e05faec47dbe627e
ChaosRifle <DC3120@gmail.com>
2024-09-11 23:18:24 -0600

    Update CONFIG_%MissionName%.lua

--- scripts/CONFIG_%MissionName%.lua
+++ scripts/CONFIG_%MissionName%.lua
@@ -5,7 +5,6 @@
 ------------------------------------------------------------------  Pre-Req's  ------------------------------------------------------------------
 --MissionName = 'defined by doScript on mission start.' used by basically anything that relies on tableIO (used as prefix for save data filenames). This needs to be configured in a missionStart trigger in miz
 --place other config scripts here that need to load before their main script. if they must load before the config, they should be loaded by the other script loader, but still configured here.
-
 ----------------------------------------------------------------- Script Config -----------------------------------------------------------------
 
 
