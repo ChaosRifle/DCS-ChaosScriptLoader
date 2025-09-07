92dbd13e3bcd1906413cbdc6f7910b0b5e374d05
Chaos <DC3120@gmail.com>
2025-09-06 19:17:12 -0600

    Update non-miz-scripts-initialization.lua

--- miz/non-miz-scripts-initialization.lua
+++ miz/non-miz-scripts-initialization.lua
@@ -1,23 +1,23 @@
 --to the guy lurking around here trying to learn from my stuff like I did with grimes work, just come ask me. -Chaos
---version:                          1.1
+local version = 1.1.1
 
-FilePath = lfs.writedir() .. [[Missions\scripts\]]                                                  --standard path
-local filePathAlternate = "C:/Users/chaosrifle/Saved Games/DCS.openbeta_server/Missions/scripts/"   --alternate path
-local filePathAlternateTwo = "C:/Users/ChaosServ/Saved Games/DCS.release_server/Missions/scripts/"  --alternate 2 path
+FilePath = lfs.writedir() .. [[Missions\scripts\]]                                                    --standard path
+local filePathAlternate = [[C:\Users\chaosrifle\Saved Games\DCS.openbeta_server\scripts\]]   --alternate path
+local filePathAlternateTwo = [[C:\Users\ChaosServ\Saved Games\DCS.release_server\Scripts\]]  --alternate 2 path
 
 env.info("Chaos Log: Loading scripts in NonMiz init", 3)
 
 if lfs and lfs.attributes then
     env.info('Chaos Log: lfs attributes exists, continuing script init')
 
-    if lfs.attributes(filePathAlternate, 'size') then
+    if lfs.attributes(FilePath, 'size') then
+        env.info('Chaos Log: Standard environment detected')
+    elseif lfs.attributes(filePathAlternate, 'size') then
         FilePath = filePathAlternate
         env.info('Chaos Log: Alternate environment detected')
     elseif lfs.attributes(filePathAlternateTwo, 'size') then
         FilePath = filePathAlternateTwo
         env.info('Chaos Log: Alternate 2 environment detected')
-    elseif lfs.attributes(FilePath, 'size') then
-        env.info('Chaos Log: Standard environment detected')
     else
         env.info('Chaos Error: Environment could not be determined.. Check filepaths for init script or script storage location')
         trigger.action.outText("Environment could not be determined.. Check filepaths for init script or script storage location", 10000)
